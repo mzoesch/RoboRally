24a7d1fa04d4a36e1ce1b0db14c5911449dfa7bb
mzosch <magnus.zoeschinger@zoeschinger.de>
2023-11-16 19:06:59 +0100

    Added package information

---
+++ src/main/java/sep/server/json/package-info.java
@@ -0,0 +1,2 @@
+/** Handles, how the server should understand the JSON. Does not actually contain any sort of logic. */
+package sep.server.json;
---
+++ src/main/java/sep/view/json/package-info.java
@@ -0,0 +1,2 @@
+/** Handles, how the client should understand the JSON. Does not actually contain any sort of logic. */
+package sep.view.json;
