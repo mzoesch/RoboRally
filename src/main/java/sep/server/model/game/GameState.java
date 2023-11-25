2841e08b729da33de7bd445ba57ba147fe5aacad
mzoesch <magnus.zoeschinger@zoeschinger.de>
2023-11-25 21:34:00 +0100

    Removed deprecated Method

--- src/main/java/sep/server/model/game/GameState.java
+++ src/main/java/sep/server/model/game/GameState.java
@@ -2,8 +2,6 @@ package sep.server.model.game;
 
 import sep.server.viewmodel.PlayerController;
 
-import org.json.JSONObject;
-
 /**
  * High-level supervisor for the entirety of a session. It manages the creation, destruction and activation of
  * Game Modes. It is persistent throughout the life-time of a session.
@@ -42,13 +40,20 @@ public class GameState
         return this.bGameStarted;
     }
 
-    /**
-     * Should return a JSON object that represents the state of the game to replicate to all clients.
-     * They will have to figure out how to interpret it and update the view accordingly.
-     */
-    public JSONObject getUpdatedState()
+    public static String[] getAvailableCourses()
     {
-        return new JSONObject();
+        return AVAILABLE_COURSES;
+    }
+
+    public String getCourseName()
+    {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName)
+    {
+        this.courseName = courseName;
+        return;
     }
 
     // endregion Getters and Setters
