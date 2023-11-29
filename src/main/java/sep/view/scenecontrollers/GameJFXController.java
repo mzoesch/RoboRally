e9691ec23b2c1672a055a23b8284c33620226779
mzoesch <magnus.zoeschinger@zoeschinger.de>
2023-11-29 16:33:41 +0100

    Made Course Fullscreen

--- src/main/java/sep/view/scenecontrollers/GameJFXController.java
+++ src/main/java/sep/view/scenecontrollers/GameJFXController.java
@@ -1,5 +1,6 @@
 package sep.view.scenecontrollers;
 
+import javafx.scene.control.ScrollPane;
 import sep.view.clientcontroller.EGameState;
 import sep.view.clientcontroller.RemotePlayer;
 import sep.view.viewcontroller.Tile;
@@ -20,13 +21,60 @@ public class GameJFXController
 {
     private static final Logger l = LogManager.getLogger(GameJFXController.class);
 
+    @FXML private AnchorPane masterContainer;
     @FXML private HBox playerContainer;
     @FXML private AnchorPane courseContainer;
+    @FXML private ScrollPane courseScrollPane;
+    @FXML private AnchorPane courseScrollPaneContent;
+
+    private int tileDimensions;
+    private static final int resizeAmount = 10;
+
+    public GameJFXController()
+    {
+        super();
+        this.tileDimensions = ViewLauncher.TILE_DIMENSIONS;
+        return;
+    }
 
     @FXML
     private void initialize()
     {
+        this.courseScrollPane.setFitToWidth(true);
+        this.courseScrollPane.setFitToHeight(true);
+        this.courseScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        this.courseScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+
+        this.courseScrollPane.widthProperty().addListener((obs, oldVal, newVal) ->
+        {
+            // TODO Only update translations not the whole course.
+            this.updateCourse();
+            return;
+        });
+
         this.updateView();
+
+        this.masterContainer.setOnKeyPressed(e ->
+        {
+            // TODO Add min max
+            switch (e.getCode())
+            {
+                /* Zoom in. */
+                case W:
+                    this.tileDimensions += GameJFXController.resizeAmount;
+                    this.updateCourse();
+                    break;
+
+                /* Zoom out. */
+                case S:
+                    this.tileDimensions -= GameJFXController.resizeAmount;
+                    this.updateCourse();
+                    break;
+            }
+            return;
+
+        });
+
         return;
     }
 
@@ -50,7 +98,7 @@ public class GameJFXController
 
     private void updateCourse()
     {
-        this.courseContainer.getChildren().clear();
+        this.courseScrollPaneContent.getChildren().clear();
 
         if (EGameState.INSTANCE.getCurrentServerCourseJSON() == null)
         {
@@ -61,6 +109,7 @@ public class GameJFXController
         /* This is not safe at all. This only works with rectangle courses. */
         int countFiles = EGameState.INSTANCE.getCurrentServerCourseJSON().getJSONArray(0).toList().size();
         int countRanks = EGameState.INSTANCE.getCurrentServerCourseJSON().toList().size();
+        this.setStyleOfCourseViewContent();
 
         Tile[][] tiles = new Tile[countFiles][countRanks];
 
@@ -87,37 +136,41 @@ public class GameJFXController
             continue;
         }
 
+        // TODO Only works with rectangle courses
+        final double minXTranslate = tiles[0][0].getTranslateX() * this.tileDimensions + (double) ViewLauncher.VIRTUAL_SPACE_HORIZONTAL / 2;
+        final double maxXTranslate = tiles[countFiles - 1][0].getTranslateX() * this.tileDimensions + (double) ViewLauncher.VIRTUAL_SPACE_HORIZONTAL / 2;
+        final double centerXTranslate = ((this.courseScrollPane.getWidth() - maxXTranslate) - minXTranslate) / 2 - (double) this.tileDimensions / 2;
+
         /* Render */
         for (int i = 0; i < countFiles; i++)
         {
             for (int j = 0; j < countRanks; j++)
             {
                 Tile t = tiles[i][j];
-                System.out.println(t.getModifier(0).getType());
 
                 AnchorPane a = new AnchorPane();
                 a.getStyleClass().add("tile");
-                a.setTranslateX(t.getTranslateX() * ViewLauncher.TILE_DIMENSIONS);
-                a.setTranslateY(t.getTranslateY() * ViewLauncher.TILE_DIMENSIONS);
+
+                double xTranslate = t.getTranslateX() * this.tileDimensions + (double) ViewLauncher.VIRTUAL_SPACE_HORIZONTAL / 2;
+                a.setTranslateX(centerXTranslate < 0 ? xTranslate : xTranslate + centerXTranslate);
+                a.setTranslateY(t.getTranslateY() * this.tileDimensions + (double) ViewLauncher.VIRTUAL_SPACE_VERTICAL / 2);
 
                 for (int k = t.getImageViews().length - 1; k >= 0; k--)
                 {
                     ImageView iv = t.getImageViews()[k];
-                    iv.setFitHeight(ViewLauncher.TILE_DIMENSIONS);
-                    iv.setFitWidth(ViewLauncher.TILE_DIMENSIONS);
+                    iv.setFitHeight(this.tileDimensions);
+                    iv.setFitWidth(this.tileDimensions);
                     a.getChildren().add(iv);
                     continue;
                 }
 
-                this.courseContainer.getChildren().add(a);
+                this.courseScrollPaneContent.getChildren().add(a);
 
                 continue;
             }
 
             continue;
         }
-
-
 
         return;
     }
@@ -151,6 +204,26 @@ public class GameJFXController
         });
 
         return;
+    }
+
+    private void setStyleOfCourseViewContent()
+    {
+        // With current scale
+        double viewWidth = this.getCountFiles() * this.tileDimensions + ViewLauncher.VIRTUAL_SPACE_HORIZONTAL;
+        double viewHeight = this.getCountRanks() * this.tileDimensions + ViewLauncher.VIRTUAL_SPACE_VERTICAL;
+        this.courseScrollPaneContent.setStyle(String.format("-fx-background-color: #000000ff; -fx-min-width: %spx; -fx-min-height: %spx;", (int) viewWidth, (int) viewHeight));
+
+        return;
+    }
+
+    private int getCountFiles()
+    {
+        return EGameState.INSTANCE.getCurrentServerCourseJSON().getJSONArray(0).toList().size();
+    }
+
+    private int getCountRanks()
+    {
+        return EGameState.INSTANCE.getCurrentServerCourseJSON().toList().size();
     }
 
     // endregion Update View Methods
