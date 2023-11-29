5178782c8b736ce54824827399b17bfc5d22f09c
mzoesch <magnus.zoeschinger@zoeschinger.de>
2023-11-29 18:07:28 +0100

    Missing return statement

--- src/main/java/sep/server/viewmodel/ClientInstance.java
+++ src/main/java/sep/server/viewmodel/ClientInstance.java
@@ -228,7 +228,8 @@ public final class ClientInstance implements Runnable
 
         if (Objects.equals(dcrp.getType_v2(), "SetStartingPoint")) {
             l.debug("Received starting point from client.");
-
+            // TODO Here call Game to for logic and affirm the starting point.
+            return true;
         }
 
         return false;
