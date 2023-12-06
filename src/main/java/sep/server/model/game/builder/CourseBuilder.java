fdbce44a846aad62f62079dc88d4e3eb77e845bd
Julian Hofmann <jthofmann@posteo.de>
2023-12-06 14:13:30 +0100

    Fixed Lasers in Course 5B

--- src/main/java/sep/server/model/game/builder/CourseBuilder.java
+++ src/main/java/sep/server/model/game/builder/CourseBuilder.java
@@ -33,7 +33,7 @@ public class CourseBuilder {
                 return buildBoard("Test");
             }
         }
-        //TODO Notlösung, da aktuell noch kein courseName übergeben wird
+        //TODO Notlösung, falls CourseName nicht richtig übergeben wird
         ArrayList<ArrayList<Tile>> boardStartA = buildBoard("StartA");
         ArrayList<ArrayList<Tile>> board5B = buildBoard("5B");
         ArrayList<ArrayList<Tile>> entireCourse = appendRight(boardStartA, board5B);
@@ -545,7 +545,6 @@ public class CourseBuilder {
 
         // (3,3)
         fieldtypes.add(new Wall(new String[]{"top"}));
-        fieldtypes.add(new Laser("top", 1));
         arrayListY.add(new Tile("5B",new Coordinate(3,3), fieldtypes));
         fieldtypes = new ArrayList<>();
 
@@ -562,7 +561,6 @@ public class CourseBuilder {
 
         // (3,6)
         fieldtypes.add(new Wall(new String[]{"left"}));
-        fieldtypes.add(new Laser("left", 1));
         arrayListY.add(new Tile("5B",new Coordinate(3,6), fieldtypes));
         fieldtypes = new ArrayList<>();
 
@@ -709,7 +707,6 @@ public class CourseBuilder {
 
         // (6,3)
         fieldtypes.add(new Wall(new String[]{"right"}));
-        fieldtypes.add(new Laser("right", 1));
         arrayListY.add(new Tile("5B",new Coordinate(6,3), fieldtypes));
         fieldtypes = new ArrayList<>();
 
@@ -726,7 +723,6 @@ public class CourseBuilder {
 
         // (6,6)
         fieldtypes.add(new Wall(new String[]{"bottom"}));
-        fieldtypes.add(new Laser("bottom", 1));
         arrayListY.add(new Tile("5B",new Coordinate(6,6), fieldtypes));
         fieldtypes = new ArrayList<>();
 
