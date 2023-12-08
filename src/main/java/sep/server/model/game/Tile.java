768ea2d8a660aebbdb37ba090678c5800b0f555f
mzoesch <magnus.zoeschinger@zoeschinger.de>
2023-12-08 12:52:52 +0100

    Restructured Tile

--- src/main/java/sep/server/model/game/Tile.java
+++ src/main/java/sep/server/model/game/Tile.java
@@ -1,25 +1,23 @@
 package sep.server.model.game;
 
-import sep.server.json.IJSONSerializable;
-import org.json.JSONObject;
-import org.json.JSONArray;
-
 import sep.server.model.game.tiles.*;
 
+import org.json.JSONArray;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Optional;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
-public class Tile {
+public class Tile
+{
     private final Coordinate coordinate;
     private Robot occupiedBy = null;
     private ArrayList<FieldType> fieldTypes;
 
-    //braucht es das onBoard-Attribut? (aus Protokoll v0.1)
-    private String isOnBoard;
+    /** Name of the board (not course) the tile is on. */
+    private final String boardName;
 
     public Tile(String onBoard, Coordinate fieldCoordinate, ArrayList<FieldType> arrayFieldTypes) {
-        isOnBoard = onBoard;
+        boardName = onBoard;
         coordinate = fieldCoordinate;
         fieldTypes = arrayFieldTypes;
     }
@@ -54,24 +52,32 @@ public class Tile {
         return false;
     }
 
-    /**
-     * Checks if the tile contains  an antenna.
-     * Returns true if any type is an instance of Antenna, otherwise returns false.
-     */
-    public boolean hasAntenna() {
-        for (FieldType fieldType : fieldTypes) {
-            if (fieldType instanceof Antenna) {
-                return true;
-            }
-        }
+
+
+    public boolean hasUnmovableRobot() {
+
+        //Noch zu implementieren
         return false;
     }
 
-    /**
-     * Checks if the tile contains  an wall.
-     * Returns true if any type is an instance of wall, otherwise returns false.
-     */
-    public boolean hasWall() {
+    // region Getters and Setters
+
+    public boolean hasAntennaModifier()
+    {
+        for (FieldType t : this.fieldTypes)
+        {
+            if (t instanceof Antenna)
+            {
+                return true;
+            }
+
+            continue;
+        }
+
+        return false;
+    }
+
+    public boolean hasWallModifier() {
         for (FieldType fieldType : fieldTypes) {
             if (fieldType instanceof Wall) {
                 return true;
@@ -80,28 +86,22 @@ public class Tile {
         return false;
     }
 
-    public boolean hasUnmovableRobot() {
-
-        //Noch zu implementieren
-        return true;
+    public String getBoardName()
+    {
+        return this.boardName;
     }
 
-    public JSONArray toJSON() {
-        JSONArray tileInfo = new JSONArray();
+    public JSONArray toJSON()
+    {
+        JSONArray j = new JSONArray();
+        for (FieldType f : this.fieldTypes)
+        {
+            j.put(f.toJSON(this.boardName));
+        }
 
-            for (int i = 0; i < fieldTypes.size(); i++) {
-                tileInfo.put(fieldTypes.get(i).toJSON(isOnBoard));
-            }
-        return tileInfo;
+        return j;
     }
 
-    public String getIsOnBoard() {
-        return isOnBoard;
-    }
+    // endregion Getters and Setters
+
 }
-
-
-
-
-
-
