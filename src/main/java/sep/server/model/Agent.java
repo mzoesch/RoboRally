915692f08cba8cb32020af6de7eaf7c930887a5b
mzoesch <magnus.zoeschinger@zoeschinger.de>
2023-12-11 17:18:43 +0100

    Added basic information for agent

--- src/main/java/sep/server/model/Agent.java
+++ src/main/java/sep/server/model/Agent.java
@@ -1,23 +1,52 @@
 package sep.server.model;
 
+import sep.server.viewmodel.Session;
+import sep.server.model.game.Player;
+
 public class Agent implements IOwnershipable
 {
+    public static final String AGENT_PREFIX = "[BOT]";
+    public static final String[] AGENT_NAMES = new String[] { "Martinez", "Anderson", "Reynolds", "Thompson", "Mitchell", "Parker", "Turner", "Bennett", "Foster", "Ramirez" };
+
+    private final String agentName;
+    private final int agentID;
+    private final Session session;
+
+    private int figure;
+
+    private Player possessing;
+
+    public Agent(final String agentName, final int agentID, final Session session)
+    {
+        super();
+
+        this.agentName = agentName;
+        this.agentID = agentID;
+        this.session = session;
+
+        this.figure = -1;
+
+        this.possessing = null;
+
+        return;
+    }
+
     @Override
     public String getName()
     {
-        return null;
+        return this.agentName;
     }
 
     @Override
     public int getPlayerID()
     {
-        return 0;
+        return this.agentID;
     }
 
     @Override
     public int getFigure()
     {
-        return 0;
+        return this.figure;
     }
 
 }
