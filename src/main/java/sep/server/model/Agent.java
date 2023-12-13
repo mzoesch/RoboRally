d9f2d7f9cd158b540eba267d01f101b80c7c1b3c
mzoesch <magnus.zoeschinger@zoeschinger.de>
2023-12-13 20:10:01 +0100

    Added primitive logic for agent

--- src/main/java/sep/server/model/Agent.java
+++ src/main/java/sep/server/model/Agent.java
@@ -3,9 +3,17 @@ package sep.server.model;
 import sep.server.model.game.GameMode;
 import sep.server.viewmodel.Session;
 import sep.server.model.game.Player;
+import sep.server.model.game.Tile;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
 
 public class Agent implements IOwnershipable
 {
+    private static final Logger l = LogManager.getLogger(Agent.class);
+
     public static final String AGENT_PREFIX = "[BOT]";
     public static final String[] AGENT_NAMES = new String[] { "Martinez", "Anderson", "Reynolds", "Thompson", "Mitchell", "Parker", "Turner", "Bennett", "Foster", "Ramirez" };
 
@@ -28,6 +36,38 @@ public class Agent implements IOwnershipable
         this.figure = IOwnershipable.INVALID_FIGURE;
 
         this.possessing = null;
+
+        return;
+    }
+
+    public void evaluateStartingPoint()
+    {
+        l.debug("Agent {} is evaluating starting point.", this.agentID);
+        final Tile t;
+        if ((t = this.possessing.getPlayerRobot().getCourse().getNextFreeStartingPoint()) != null)
+        {
+            l.debug("Agent {} evaluated starting point at {}.", this.agentID, t.getCoordinate().toString());
+            this.getAuthGameMode().setStartingPoint(this, t.getCoordinate().getX(), t.getCoordinate().getY());
+            return;
+        }
+
+        l.error("Agent {} could not evaluate starting point.", this.agentID);
+
+        return;
+    }
+
+    public void evaluateProgrammingPhase()
+    {
+        l.debug("Agent {} is evaluating for the current programming phase.", this.agentID);
+
+        /* Very, very primitive. Just a framework for now. Open for later construction. */
+        for (int i = 0; i < 5; i++)
+        {
+            this.possessing.setCardToRegister(this.possessing.getPlayerHand().get(i).getCardType(), i);
+            continue;
+        }
+
+        l.debug("Agent {} evaluated for the current programming phase. The determined cards are: {}.", this.agentID, Arrays.toString(this.possessing.getRegisters()));
 
         return;
     }
