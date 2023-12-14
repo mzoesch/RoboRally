220574dbd93054f6ea95dc457d2d9c1f15beee71
mzoesch <magnus.zoeschinger@zoeschinger.de>
2023-12-14 17:52:16 +0100

    Added logic to handle min remote players request

--- src/main/java/sep/wrapper/ServerConfigJFXController.java
+++ src/main/java/sep/wrapper/ServerConfigJFXController.java
@@ -2,25 +2,28 @@ package sep.wrapper;
 
 import sep.EPort;
 import sep.EArgs;
+import sep.server.model.game.GameState;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class ServerConfigJFXController
+public final class ServerConfigJFXController
 {
     @FXML private Label alertLabel;
     @FXML private TextField serverIPTextField;
     @FXML private TextField serverPortTextField;
+    @FXML private TextField minRemotePlayersTextField;
 
     @FXML
-    protected void onStartServerBtn(ActionEvent actionEvent)
+    private void onStartServerBtn(final ActionEvent actionEvent)
     {
         this.alertLabel.setText("");
 
-        final String ip = serverIPTextField.getText();
-        final String port = serverPortTextField.getText();
+        final String ip = this.serverIPTextField.getText();
+        final String port = this.serverPortTextField.getText();
+        final String minRemotePlayers = this.minRemotePlayersTextField.getText();
 
         if (!(port.isEmpty() || port.isBlank()))
         {
@@ -38,13 +41,24 @@ public class ServerConfigJFXController
             EArgs.setCustomServerIP(ip);
         }
 
+        if (!(minRemotePlayers.isEmpty() || minRemotePlayers.isBlank()))
+        {
+            if (Integer.parseInt(minRemotePlayers) < GameState.MIN_CONTROLLERS_ALLOWED || Integer.parseInt(minRemotePlayers) > GameState.MAX_CONTROLLERS_ALLOWED)
+            {
+                this.showAlert("Invalid minimum remote players number.");
+                return;
+            }
+
+            EArgs.setCustomMinRemotePlayers(Integer.parseInt(minRemotePlayers));
+        }
+
         EArgs.setMode(EArgs.SERVER);
         Wrapper.exitWrapper();
         return;
     }
 
     @FXML
-    protected void onCancelBtn(ActionEvent actionEvent)
+    private void onCancelBtn(final ActionEvent actionEvent)
     {
         Wrapper.loadWrapperMenu();
         return;
@@ -55,6 +69,7 @@ public class ServerConfigJFXController
     {
         this.serverIPTextField.setPromptText(EArgs.PREF_SERVER_IP);
         this.serverPortTextField.setPromptText(String.valueOf(EArgs.PREF_SERVER_PORT.i));
+        this.minRemotePlayersTextField.setPromptText(String.valueOf(GameState.DEFAULT_MIN_REMOTE_PLAYER_COUNT_TO_START));
 
         this.serverPortTextField.textProperty().addListener((observable, o, t1) ->
         {
@@ -66,10 +81,25 @@ public class ServerConfigJFXController
             return;
         });
 
+        this.minRemotePlayersTextField.textProperty().addListener((observable, o, t1) ->
+        {
+            if (!t1.matches("\\d*"))
+            {
+                this.minRemotePlayersTextField.setText(t1.replaceAll("[^\\d]", ""));
+            }
+
+            if (Integer.parseInt(t1) < GameState.MIN_CONTROLLERS_ALLOWED || Integer.parseInt(t1) > GameState.MAX_CONTROLLERS_ALLOWED)
+            {
+                this.minRemotePlayersTextField.setText(o);
+            }
+
+            return;
+        });
+
         return;
     }
 
-    public void showAlert(String message)
+    public void showAlert(final String message)
     {
         this.alertLabel.setText(message);
         return;
