696bf26404b90a66a7c687910d4c9051fe6393cd
mzoesch <magnus.zoeschinger@zoeschinger.de>
2023-12-27 16:03:39 +0100

    Typo

--- src/main/java/sep/view/clientcontroller/ServerListener.java
+++ src/main/java/sep/view/clientcontroller/ServerListener.java
@@ -338,7 +338,7 @@ public class ServerListener implements Runnable
     {
         /* TODO Display current cards of each player in the UI. */
         final StringBuilder sb = new StringBuilder();
-        sb.append(String.format("In %s. register these cards were played: (", EGameState.INSTANCE.getCurrentRegister()));
+        sb.append(String.format("In register %s these cards were played: (", EGameState.INSTANCE.getCurrentRegister()));
         sb.append(IntStream.range(0, this.dsrp.getActiveCards().length()).mapToObj(i -> String.format("%s[Player %d played card %s]", i == 0 ? "" : ", ", this.dsrp.getPlayerIDFromActiveCardIdx(i), this.dsrp.getActiveCardFromIdx(i))).collect(Collectors.joining()));
         sb.append(").");
         l.debug(sb.toString());
