f13c1d641528ccfa6f7ce7060d1d0c887e049fc0
mzoesch <magnus.zoeschinger@zoeschinger.de>
2023-12-28 16:24:24 +0100

    Update Figure

--- src/main/java/sep/view/lib/Types.java
+++ src/main/java/sep/view/lib/Types.java
@@ -1,6 +1,6 @@
 package sep.view.lib;
 
-public class Types
+public final class Types
 {
     private Types() throws IllegalStateException
     {
@@ -9,23 +9,25 @@ public class Types
 
     public enum EFigure
     {
-        INVALID(-1),
-        HAMMER(0),
-        TRUNDLE(1),
-        SQUASH(2),
-        X90(3),
-        SPIN(4),
-        TWONKY(5),
-        TWITCH(6),
-        NUM(7);
-
-        private static final String[] FIGURE_NAMES = new String[] {"Hammer Bot", "Trundle Bot", "Squash Bot", "Hulk x90", "Spin Bot", "Twonky", "Twitch"};
+        INVALID (  -1,  "Invalid"       ),
+        HAMMER  (   0,  "Hammer Bot"    ),
+        TRUNDLE (   1,  "Trundle Bot"   ),
+        SQUASH  (   2,  "Squash Bot"    ),
+        X90     (   3,  "Hulk x90"      ),
+        SPIN    (   4,  "Spin Bot"      ),
+        TWONKY  (   5,  "Twonky"        ),
+        TWITCH  (   6,  "Twitch"        ),
+        NUM     (   7,  "FALSE USE"     )
+        ;
 
         public final int i;
+        private final String s;
 
-        private EFigure(final int i)
+        private EFigure(final int i, final String name)
         {
             this.i = i;
+            this.s = name;
+
             return;
         }
 
@@ -45,9 +47,9 @@ public class Types
         }
 
         @Override
-        public String toString() throws ArrayIndexOutOfBoundsException
+        public String toString()
         {
-           return Types.EFigure.FIGURE_NAMES[this.i];
+            return this.s;
         }
 
     }
