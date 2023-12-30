485c9502134002bd0a9094183b3e0fd95623b7e2
mzoesch <magnus.zoeschinger@zoeschinger.de>
2023-12-30 17:04:52 +0100

    Added necessary coordinates getter

--- src/main/java/sep/view/lib/RCoordinate.java
+++ src/main/java/sep/view/lib/RCoordinate.java
@@ -1,5 +1,10 @@
 package sep.view.lib;
 
+import sep.view.viewcontroller.     Tile;
+import sep.view.lib.Types.          ERotation;
+import sep.view.clientcontroller.   EGameState;
+import sep.view.clientcontroller.   RemotePlayer;
+
 public record RCoordinate(int x, int y)
 {
     public RCoordinate(final int x, final int y)
@@ -30,6 +35,46 @@ public record RCoordinate(int x, int y)
     public String toString()
     {
         return String.format("(%d, %d)", this.x, this.y);
+    }
+
+    public static boolean isOccupied(final RCoordinate c)
+    {
+        for (final RemotePlayer rp : EGameState.INSTANCE.getRemotePlayers())
+        {
+            if (rp.getFigureLocation().equals(c))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public RCoordinate getNeighbour(final ERotation rot)
+    {
+        switch (rot)
+        {
+        case NORTH:
+            return new RCoordinate(this.x, this.y - 1);
+        case EAST:
+            return new RCoordinate(this.x + 1, this.y);
+        case SOUTH:
+            return new RCoordinate(this.x, this.y + 1);
+        case WEST:
+            return new RCoordinate(this.x - 1, this.y);
+        default:
+            return null;
+        }
+    }
+
+    public static boolean exists(final RCoordinate c, final Tile[][] tiles)
+    {
+        if (tiles == null)
+        {
+            return false;
+        }
+
+        return (c.x >= 0) && (c.x < tiles.length) && (c.y >= 0) && (c.y < tiles[0].length);
     }
 
 }
