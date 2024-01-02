ec610ccc2e6bc1f11e21fb98652ee5510f7ccbea
mzoesch <magnus.zoeschinger@zoeschinger.de>
2024-01-02 17:22:58 +0100

    Styling module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,30 +1,30 @@
 module sep
 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.json;
-    requires org.apache.logging.log4j;
     requires javafx.                swing;
+    requires javafx.                controls;
+    requires javafx.                fxml;
+    requires org.                   json;
+    requires org.apache.logging.    log4j;
     requires java.                  desktop;
 
-    exports sep;
-    opens sep to javafx.fxml;
+    opens       sep     to javafx.fxml;
+    exports     sep;
 
-    opens sep.view to javafx.fxml;
-    exports sep.view;
+    opens       sep.view    to javafx.fxml;
+    exports     sep.view;
 
-    opens sep.view.scenecontrollers to javafx.fxml;
-    exports sep.view.scenecontrollers;
+    opens       sep.view.scenecontrollers   to javafx.fxml;
+    exports     sep.view.scenecontrollers;
 
-    opens sep.view.clientcontroller to javafx.fxml;
-    exports sep.view.clientcontroller;
+    opens       sep.view.clientcontroller   to javafx.fxml;
+    exports     sep.view.clientcontroller;
 
-    opens sep.view.viewcontroller to javafx.fxml;
-    exports sep.view.viewcontroller;
+    opens       sep.view.viewcontroller     to javafx.fxml;
+    exports     sep.view.viewcontroller;
 
-    opens sep.wrapper to javafx.fxml;
-    exports sep.wrapper;
+    opens       sep.wrapper     to javafx.fxml;
+    exports     sep.wrapper;
 
-    exports sep.view.json;
-    exports sep.view.lib;
+    exports     sep.view.json;
+    exports     sep.view.lib;
 }
