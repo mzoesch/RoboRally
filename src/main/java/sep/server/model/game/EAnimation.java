2e54187086bd65dbaab7a068958af43b086178ca
mzoesch <magnus.zoeschinger@zoeschinger.de>
2024-01-03 19:42:12 +0100

    Added EAnimation.java

---
+++ src/main/java/sep/server/model/game/EAnimation.java
@@ -0,0 +1,44 @@
+package sep.server.model.game;
+
+import java.util.Objects;
+
+public enum EAnimation
+{
+    BLUE_CONVEYOR_BELT  (   "BlueConveyorBelt"     ),
+    GREEN_CONVEYOR_BELT (   "GreenConveyorBelt"    ),
+    PUSH_PANEL          (   "PushPanel"            ),
+    GEAR                (   "Gear"                 ),
+    CHECK_POINT         (   "CheckPoint"           ), /* TODO What anim should we play here?? */
+    PLAYER_SHOOTING     (   "PlayerShooting"       ),
+    WALL_SHOOTING       (   "WallShooting"         ),
+    ENERGY_SPACE        (   "EnergySpace"          ),
+    ;
+
+    private final String s;
+
+    private EAnimation(final String s)
+    {
+        this.s = s;
+        return;
+    }
+
+    public static EAnimation fromString(final String s)
+    {
+        for (final EAnimation a : EAnimation.values())
+        {
+            if (Objects.equals(a.s, s))
+            {
+                return a;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public String toString()
+    {
+        return this.s;
+    }
+
+}
