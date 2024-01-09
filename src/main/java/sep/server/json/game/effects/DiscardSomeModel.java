9afbc48c43469cbf6a8438c4b429cda00f652cc9
jaxabykowska <jaxabykowska@cip.ifi.lmu.de>
2024-01-09 15:47:52 +0100

    Added DiscardSomeModel  JSON messages

---
+++ src/main/java/sep/server/json/game/effects/DiscardSomeModel.java
@@ -0,0 +1,24 @@
+package sep.server.json.game.effects;
+
+import org.json.JSONObject;
+import sep.server.json.AModel;
+import sep.server.viewmodel.ClientInstance;
+
+public class DiscardSomeModel extends AModel {
+
+    private final String cards;
+
+    public DiscardSomeModel(ClientInstance ci, String cards) {
+        super(ci);
+        this.cards = cards;
+    }
+
+    @Override
+    public JSONObject toJSON() {
+        JSONObject j = new JSONObject();
+        j.put("messageType", "DiscardSome");
+        j.put("messageBody", new JSONObject().put("cards", this.cards));
+
+        return j;
+    }
+}
