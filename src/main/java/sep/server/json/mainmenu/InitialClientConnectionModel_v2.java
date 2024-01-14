1075fc65f6f2b11c29f8249ddc1e5c06c80a650b
mzoesch <magnus.zoeschinger@zoeschinger.de>
2024-01-14 15:36:35 +0100

    Initial Client Connection now supports remote agent detection

--- src/main/java/sep/server/json/mainmenu/InitialClientConnectionModel_v2.java
+++ src/main/java/sep/server/json/mainmenu/InitialClientConnectionModel_v2.java
@@ -1,22 +1,22 @@
 package sep.server.json.mainmenu;
 
-import sep.Types;
-import sep.server.viewmodel.ClientInstance;
+import sep.                         Types;
+import sep.server.viewmodel.        ClientInstance;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.json.                    JSONException;
+import org.json.                    JSONObject;
+import org.apache.logging.log4j.    LogManager;
+import org.apache.logging.log4j.    Logger;
 
 /** Handles the initial client server handshake. */
 public class InitialClientConnectionModel_v2
 {
     private static final Logger l = LogManager.getLogger(InitialClientConnectionModel_v2.class);
 
-    private final ClientInstance ci;
-    private JSONObject response;
+    private final ClientInstance    ci;
+    private JSONObject              response;
 
-    public InitialClientConnectionModel_v2(ClientInstance ci)
+    public InitialClientConnectionModel_v2(final ClientInstance ci)
     {
         super();
         this.ci = ci;
@@ -25,9 +25,9 @@ public class InitialClientConnectionModel_v2
 
     public void sendProtocolVersion()
     {
-        JSONObject j = new JSONObject();
-        j.put("messageType", "HelloClient");
-        j.put("messageBody", new JSONObject().put("protocol", String.format("Version %s", Types.EProps.VERSION.toString())));
+        final JSONObject j = new JSONObject();
+        j.put(  "messageType",  "HelloClient"                                                                                   );
+        j.put(  "messageBody",  new JSONObject().put("protocol", String.format("Version %s", Types.EProps.VERSION.toString()))  );
 
         if (this.ci.sendRemoteRequest(j))
         {
@@ -36,14 +36,14 @@ public class InitialClientConnectionModel_v2
         }
 
         l.fatal("Failed to send protocol version to client.");
-        this.ci.handleDisconnect();
+        this.ci.disconnect();
 
         return;
     }
 
     public void waitForProtocolVersionConfirmation() throws JSONException
     {
-        String response = this.ci.waitForResponse();
+        final String response = this.ci.waitForResponse();
         if (response == null)
         {
             l.error("Failed to receive response from client");
@@ -54,11 +54,11 @@ public class InitialClientConnectionModel_v2
         return;
     }
 
-    public void sendWelcome(int playerID)
+    public void sendWelcome(final int playerID)
     {
-        JSONObject j = new JSONObject();
-        j.put("messageType", "Welcome");
-        j.put("messageBody", new JSONObject().put("clientID", playerID));
+        final JSONObject j = new JSONObject();
+        j.put(  "messageType",  "Welcome"                                   );
+        j.put(  "messageBody",  new JSONObject().put("clientID", playerID)  );
 
         if (this.ci.sendRemoteRequest(j))
         {
@@ -67,7 +67,7 @@ public class InitialClientConnectionModel_v2
         }
 
         l.fatal("Failed to send welcome to client.");
-        this.ci.handleDisconnect();
+        this.ci.disconnect();
 
         return;
     }
@@ -86,10 +86,10 @@ public class InitialClientConnectionModel_v2
                 return false;
             }
 
-            JSONObject messageBody = this.response.getJSONObject("messageBody");
+            final JSONObject body = this.response.getJSONObject("messageBody");
 
             // Maybe we should check for legacy protocol versions in the future?
-            if (!messageBody.getString("protocol").equals(String.format("Version %s", Types.EProps.VERSION.toString())))
+            if (!body.getString("protocol").equals(String.format("Version %s", Types.EProps.VERSION.toString())))
             {
                 l.fatal("Client protocol version is not compatible with server protocol version.");
                 return false;
@@ -97,13 +97,32 @@ public class InitialClientConnectionModel_v2
 
             return true;
         }
-        catch (JSONException e)
+        catch (final JSONException e)
         {
             l.error("Failed to parse response from client");
             l.error(e.getMessage());
             return false;
         }
 
+    }
+
+    public boolean isRemoteAgent()
+    {
+        if (this.response == null)
+        {
+            return false;
+        }
+
+        try
+        {
+            return this.response.getJSONObject("messageBody").getBoolean("isAI");
+        }
+        catch (final JSONException e)
+        {
+            l.error("Failed to parse response from client");
+            l.error(e.getMessage());
+            return false;
+        }
     }
 
     public String getSessionID()
@@ -117,7 +136,7 @@ public class InitialClientConnectionModel_v2
         {
             return this.response.getJSONObject("messageBody").getString("group");
         }
-        catch (JSONException e)
+        catch (final JSONException e)
         {
             l.error("Failed to parse response from client");
             l.error(e.getMessage());
@@ -126,4 +145,3 @@ public class InitialClientConnectionModel_v2
     }
 
 }
-
