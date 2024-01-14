6a4ad3ff45e42ae49a350886b0aaaaa1ef15ef64
mzoesch <magnus.zoeschinger@zoeschinger.de>
2024-01-14 15:18:55 +0100

    Styling

--- src/main/java/sep/server/viewmodel/ServerListener.java
+++ src/main/java/sep/server/viewmodel/ServerListener.java
@@ -1,11 +1,11 @@
 package sep.server.viewmodel;
 
-import sep.server.model.EServerInformation;
+import sep.server.model.            EServerInformation;
 
-import java.io.IOException;
-import java.net.Socket;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.io.                     IOException;
+import org.apache.logging.log4j.    LogManager;
+import org.apache.logging.log4j.    Logger;
+import java.net.                    Socket;
 
 /**
  * We create a special object for listening to the server socket. This object is instantiated on the main thread.
@@ -28,13 +28,13 @@ public final class ServerListener
         while (true)
         {
             l.debug("Listening on port {} for client connection . . .", EServerInformation.INSTANCE.getPort());
-            Socket client = EServerInformation.INSTANCE.getServerSocket().accept();
+            final Socket client = EServerInformation.INSTANCE.getServerSocket().accept();
             l.debug("Accepted connection from {}.", client.getInetAddress().getHostAddress());
 
-            // TODO This is a little bit weird, the Client Instance should extend Thread not implement it. Fix.
-            ClientInstance ci = new ClientInstance(client);
-            Thread t = new Thread(ci);
-            ci.thread = t;
+            /* TODO This is a little bit weird, the Client Instance should extend Thread not implement it. Fix. */
+            final ClientInstance ci     = new ClientInstance(client);
+            final Thread t              = new Thread(ci);
+            ci.setThread(t);
             t.start();
 
             continue;
