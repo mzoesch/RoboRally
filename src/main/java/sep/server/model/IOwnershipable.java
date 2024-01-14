b41c13bc14f299f5c5fc3ac446b0ed50fad77eb7
mzoesch <magnus.zoeschinger@zoeschinger.de>
2024-01-14 15:05:54 +0100

    Styling

--- src/main/java/sep/server/model/IOwnershipable.java
+++ src/main/java/sep/server/model/IOwnershipable.java
@@ -1,19 +1,19 @@
 package sep.server.model;
 
-import sep.server.model.game.GameMode;
-import sep.server.model.game.Player;
+import sep.server.model.game.   GameMode;
+import sep.server.model.game.   Player;
 
 public interface IOwnershipable
 {
-    public static final int INVALID_FIGURE = -1;
+    public static final int     INVALID_FIGURE                  = -1;
 
-    public String getName();
-    public int getPlayerID();
-    public void setFigure(final int figure);
-    public int getFigure();
+    public String               getName();
+    public int                  getPlayerID();
+    public void                 setFigure(final int figure);
+    public int                  getFigure();
 
-    public GameMode getAuthGameMode();
+    public GameMode             getAuthGameMode();
 
-    public void setPlayer(final Player p);
+    public void                 setPlayer(final Player p);
 
 }
