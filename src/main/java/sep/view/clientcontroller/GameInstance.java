b5eb01498226544aa4b7ce58e850c2666a720f7c
mzoesch <magnus.zoeschinger@zoeschinger.de>
2024-01-15 12:27:30 +0100

    Split game instance

--- src/main/java/sep/view/clientcontroller/GameInstance.java
+++ src/main/java/sep/view/clientcontroller/GameInstance.java
@@ -1,147 +1,42 @@
 package sep.view.clientcontroller;
 
-import sep.view.json.mainmenu.InitialClientConnectionModel;
+import sep.view.json.mainmenu.      InitialClientConnectionModel;
 
-import javafx.application.Platform;
-import java.io.IOException;
-
-import org.json.JSONObject;
-import org.json.JSONException;
-import sep.view.viewcontroller.ViewSupervisor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import javafx.application.          Platform;
+import java.io.                     IOException;
+import org.json.                    JSONObject;
+import org.json.                    JSONException;
+import org.apache.logging.log4j.    LogManager;
+import org.apache.logging.log4j.    Logger;
 
 /**
  * High-level manager object for an instance of the running game. Spawned at game creation and not destroyed until
  * the game is killed. It implements important methods that affect the game as a whole, such as connecting
  * to the server or shutting down.
  */
-public final class GameInstance
+public abstract sealed class GameInstance permits GI_Agent, GI_Human
 {
     private static final Logger l = LogManager.getLogger(GameInstance.class);
 
-    public static final int MAX_PLAYER_NAME_LENGTH      = 16;
-    private static final int MAX_CONNECTION_TRIES       = 3;
+    public static final int MAX_PLAYER_NAME_LENGTH = 16;
 
-    private GameInstance()
+    protected GameInstance()
     {
         super();
 
         if (EClientInformation.INSTANCE.getJFXInstance() == null)
         {
             EClientInformation.INSTANCE.setJFXInstance(this);
-
-            if (EClientInformation.INSTANCE.isAgent())
-            {
-                l.info("Creating Game Instance for main thread (agent). Connecting to server.");
-                GameInstance.runAgentLoop();
-                return;
-            }
-
-            l.info("Creating Game Instance for main thread (JFX), constructing window.");
-            ViewSupervisor.run();
             return;
         }
 
-        l.info("Creating Game Instance for thread.");
+        l.fatal("Creating Game Instance for thread even though one already exists.");
+        GameInstance.kill();
 
         return;
     }
 
-    private static void runAgentLoop()
-    {
-        int tries = 0;
-        while (tries < GameInstance.MAX_CONNECTION_TRIES)
-        {
-            try
-            {
-                if (GameInstance.connectToServer())
-                {
-                    l.info("Agent successfully connected to server.");
-                    break;
-                }
-
-                l.error("Some unknown error occurred while connecting to server.");
-
-                if (tries > GameInstance.MAX_CONNECTION_TRIES - 2)
-                {
-                    break;
-                }
-
-                l.info("Retrying in 5 seconds. Failed {} times. Quitting after {} tries.", tries + 1, GameInstance.MAX_CONNECTION_TRIES);
-                try
-                {
-                    Thread.sleep(5_000);
-                }
-                catch (final InterruptedException e)
-                {
-                    l.error("Interrupted while waiting to retry.");
-                    l.error(e.getMessage());
-                    return;
-                }
-
-                tries++;
-
-                continue;
-            }
-            catch (final IOException e)
-            {
-                l.error("Interrupted while waiting to retry.");
-                l.error(e.getMessage());
-                l.info("Retrying in 5 seconds. Failed {} times. Quitting after {} tries.", tries + 1, GameInstance.MAX_CONNECTION_TRIES);
-
-                try
-                {
-                    Thread.sleep(5_000);
-                }
-                catch (final InterruptedException e1)
-                {
-                    l.error("Failed to sleep.");
-                    l.error(e1.getMessage());
-                    return;
-                }
-
-                tries++;
-
-                continue;
-            }
-        }
-
-        if (!EClientInformation.INSTANCE.hasServerConnection())
-        {
-            l.fatal("Failed to connect to server.");
-            l.fatal("Requesting shutdown.");
-            GameInstance.kill();
-            return;
-        }
-
-        try
-        {
-            if (!GameInstance.connectToSessionPostLogin())
-            {
-                l.fatal("Failed to connect to session.");
-                l.fatal("Requesting shutdown.");
-                GameInstance.kill();
-                return;
-            }
-        }
-        catch (final IOException | JSONException e)
-        {
-            l.fatal("Failed to connect to session.");
-            l.fatal("Requesting shutdown.");
-            GameInstance.kill();
-            return;
-        }
-
-        return;
-    }
-
-    public static void run()
-    {
-        //noinspection InstantiationOfUtilityClass
-        new GameInstance();
-        return;
-    }
+    public abstract void run();
 
     public static void kill()
     {
@@ -252,6 +147,11 @@ public final class GameInstance
         }
 
         return res;
+    }
+
+    public static GameInstance createGameInstance()
+    {
+        return EClientInformation.INSTANCE.isAgent() ? new GI_Agent() : new GI_Human();
     }
 
 }
