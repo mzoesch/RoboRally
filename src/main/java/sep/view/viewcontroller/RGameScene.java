b5316aba41c9b619caa8f7d79951a6bc19cb81ad
mzoesch <magnus.zoeschinger@zoeschinger.de>
2024-01-15 14:01:34 +0100

    Wrong name capitalization

--- src/main/java/sep/view/viewcontroller/RGameScene.java
+++ src/main/java/sep/view/viewcontroller/RGameScene.java
@@ -5,26 +5,26 @@ import javafx.scene.layout.Pane;
 /**
  * Holds useful information about a rendered screen.
  *
- * @param ID            The ID of the screen.
+ * @param id            The ID of the screen.
  * @param screen        The actual scene (pane).
  * @param controller    The controller of the scene.
  * @param fallback      The fallback scene to go to when this screen is destroyed.
  * @param <T>           The type of the controller.
  */
-public record RGameScene<T>(String ID, Pane screen, T controller, String fallback)
+public record RGameScene<T>(String id, Pane screen, T controller, String fallback)
 {
-    public RGameScene(final String ID, final Pane screen, final T controller, String fallback)
+    public RGameScene(final String id, final Pane screen, final T controller, String fallback)
     {
-        this.ID = ID;
-        this.screen = screen;
-        this.controller = controller;
-        this.fallback = fallback;
+        this.id             = id;
+        this.screen         = screen;
+        this.controller     = controller;
+        this.fallback       = fallback;
 
         return;
     }
 
     /**
-     * If a screen is destroyed the {@link SceneController} will try its best to get to back to
+     * If a screen is destroyed the {@link SceneController} will try its best to get back to
      * the screen rendered before. If this fails, the game will force quit within an instant.
      */
     public boolean hasFallback()
