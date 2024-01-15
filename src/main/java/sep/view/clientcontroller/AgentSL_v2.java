af0c7183f03fa50ce9033b167b512cb4ad4f6bb0
mzoesch <magnus.zoeschinger@zoeschinger.de>
2024-01-15 18:23:51 +0100

    Agents can now set their start pos

--- src/main/java/sep/view/clientcontroller/AgentSL_v2.java
+++ src/main/java/sep/view/clientcontroller/AgentSL_v2.java
@@ -2,13 +2,276 @@ package sep.view.clientcontroller;
 
 import sep.view.json.lobby.         PlayerValuesModel;
 import sep.view.json.lobby.         SetStatusModel;
+import sep.view.json.game.          SetStartingPointModel;
+import sep.view.lib.                EGamePhase;
+import sep.view.lib.                RCoordinate;
 
 import org.json.                    JSONException;
 import org.json.                    JSONObject;
+import org.json.                    JSONArray;
 import org.apache.logging.log4j.    LogManager;
 import org.apache.logging.log4j.    Logger;
 import java.io.                     BufferedReader;
 import java.util.                   Objects;
+
+class TileModifier
+{
+    private final JSONObject modifier;
+
+    public TileModifier(final JSONObject modifier)
+    {
+        this.modifier = modifier;
+        return;
+    }
+
+    public String getType()
+    {
+        return this.modifier.getString("type");
+    }
+
+}
+
+class Tile
+{
+    private final JSONArray     tile;
+    private final RCoordinate   location;
+
+    public Tile (final JSONArray tile, final RCoordinate location)
+    {
+        this.tile       = tile;
+        this.location   = location;
+
+        return;
+    }
+
+    public RCoordinate getLocation()
+    {
+        return location;
+    }
+
+    public TileModifier getModifier(final int idx)
+    {
+        return new TileModifier(this.tile.getJSONObject(idx));
+    }
+
+    public boolean isStartPoint()
+    {
+        for (int i = 0; i < this.tile.length(); ++i)
+        {
+            if (Objects.equals(this.getModifier(i).getType(), "StartPoint"))
+            {
+                return true;
+            }
+
+            continue;
+        }
+
+        return false;
+    }
+
+    public JSONArray getJSON()
+    {
+        return this.tile;
+    }
+
+}
+
+interface ICourse
+{
+    public abstract     Tile[][]        getTiles();
+    public abstract     int             getFiles();
+    public abstract     int             getRanks();
+
+    public abstract     RCoordinate getNextFreeStartPoint();
+}
+
+enum ECourse implements ICourse
+{
+    INSTANCE;
+
+    private enum ECourseImpl implements ICourse
+    {
+        INSTANCE;
+
+        private static final Logger l = LogManager.getLogger(ECourseImpl.class);
+
+        private JSONObject course;
+
+        private ECourseImpl()
+        {
+            this.course = null;
+            return;
+        }
+
+        public void setCourse(final JSONObject course)
+        {
+            this.course = course;
+            return;
+        }
+
+        private JSONArray getTilesAsJSON() throws JSONException
+        {
+            return this.course.getJSONObject("messageBody").getJSONArray("gameMap");
+        }
+
+        @Override
+        public Tile[][] getTiles() throws JSONException
+        {
+            final Tile[][] course = new Tile[this.getFiles()][this.getRanks()];
+
+            for (int file = 0; file < this.getFiles(); ++file)
+            {
+                for (int rank = 0; rank < this.getRanks(); ++rank)
+                {
+                    course[file][rank] = new Tile(this.getTilesAsJSON().getJSONArray(file).getJSONArray(rank), new RCoordinate(file, rank));
+                    continue;
+                }
+
+                continue;
+            }
+
+            return course;
+        }
+
+        @Override
+        public int getFiles() throws JSONException
+        {
+            return this.getTilesAsJSON().toList().size();
+        }
+
+        @Override
+        public int getRanks() throws JSONException
+        {
+            return this.getTilesAsJSON().getJSONArray(0).toList().size();
+        }
+
+        @Override
+        public RCoordinate getNextFreeStartPoint() throws JSONException
+        {
+            final Tile[][] tiles = this.getTiles();
+
+            for (int file = 0; file < this.getFiles(); ++file)
+            {
+                for (int rank = 0; rank < this.getRanks(); ++rank)
+                {
+                    if (tiles[file][rank].isStartPoint())
+                    {
+                        boolean bFree = true;
+
+                        for (int i = 0; i < EGameState.INSTANCE.getRemotePlayers().length; ++i)
+                        {
+                            if (EGameState.INSTANCE.getRemotePlayers()[i].getStartingPosition() == null)
+                            {
+                                continue;
+                            }
+
+                            if (Objects.equals(EGameState.INSTANCE.getRemotePlayers()[i].getStartingPosition(), tiles[file][rank].getLocation()))
+                            {
+                                bFree = false;
+                                break;
+                            }
+
+                            continue;
+                        }
+
+                        if (bFree)
+                        {
+                            return tiles[file][rank].getLocation();
+                        }
+
+                        continue;
+                    }
+
+                    continue;
+                }
+
+                continue;
+            }
+
+            l.fatal("Failed to find a free starting points.");
+            GameInstance.kill();
+
+            return null;
+        }
+
+        public JSONObject getCourse()
+        {
+            return course;
+        }
+
+    }
+
+    private static final Logger l = LogManager.getLogger(ECourse.class);
+
+    private ECourse()
+    {
+        return;
+    }
+
+    public void setCourse(final JSONObject course)
+    {
+        ECourseImpl.INSTANCE.setCourse(course);
+        return;
+    }
+
+    private boolean isCourseMissing()
+    {
+        if (ECourseImpl.INSTANCE.getCourse() == null)
+        {
+            l.fatal("Course is null. Cannot get tiles.");
+            GameInstance.kill();
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public Tile[][] getTiles() throws JSONException
+    {
+        if (this.isCourseMissing())
+        {
+            return null;
+        }
+
+        return ECourseImpl.INSTANCE.getTiles();
+    }
+
+    @Override
+    public int getFiles() throws JSONException
+    {
+        if (this.isCourseMissing())
+        {
+            return -1;
+        }
+
+        return ECourseImpl.INSTANCE.getFiles();
+    }
+
+    @Override
+    public int getRanks() throws JSONException
+    {
+        if (this.isCourseMissing())
+        {
+            return -1;
+        }
+
+        return ECourseImpl.INSTANCE.getRanks();
+    }
+
+    @Override
+    public RCoordinate getNextFreeStartPoint() throws JSONException
+    {
+        if (this.isCourseMissing())
+        {
+            return null;
+        }
+
+        return ECourseImpl.INSTANCE.getNextFreeStartPoint();
+    }
+
+}
+
 
 /**
  * The master class of an agent. An object of this class must always run on the main thread.
@@ -86,20 +349,45 @@ public final class AgentSL_v2 extends ServerListener
     protected boolean onGameStart() throws JSONException
     {
         l.debug("Game start received.");
-        this.serverCourse = this.dsrp.request();
+        ECourse.INSTANCE.setCourse(this.dsrp.request());
         return true;
     }
 
     @Override
     protected boolean onPhaseChange() throws JSONException
     {
-        return false;
+        l.debug("Game phase has changed. New phase: {}", EGamePhase.fromInt(this.dsrp.getPhase()));
+        EGameState.INSTANCE.setCurrentPhase(EGamePhase.fromInt(this.dsrp.getPhase()));
+        return true;
     }
 
     @Override
     protected boolean onPlayerTurnChange() throws JSONException
     {
-        return false;
+        EGameState.INSTANCE.setCurrentPlayer(this.dsrp.getPlayerID(), true);
+
+        if (EGameState.INSTANCE.getCurrentPhase() == EGamePhase.REGISTRATION)
+        {
+            if (this.dsrp.getPlayerID() == EClientInformation.INSTANCE.getPlayerID())
+            {
+                final RCoordinate location = ECourse.INSTANCE.getNextFreeStartPoint();
+                if (location == null)
+                {
+                    l.fatal("Failed to get free starting point.");
+                    GameInstance.kill();
+                    return false;
+                }
+
+                l.debug("Sending starting point: {}.", location.toString());
+                new SetStartingPointModel(location.x(), location.y()).send();
+
+                return true;
+            }
+
+            return true;
+        }
+
+        return true;
     }
 
     @Override
