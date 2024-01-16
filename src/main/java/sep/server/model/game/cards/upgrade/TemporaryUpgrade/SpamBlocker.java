ddd1783776735910dd8b498b019fadcb749c13ae
Jakob Zebhauser <jakob.zebhauser@web.de>
2024-01-16 12:22:44 +0100

    UpgradeKarte SpamBlocker

--- src/main/java/sep/server/model/game/cards/upgrade/TemporaryUpgrade/SpamBlocker.java
+++ src/main/java/sep/server/model/game/cards/upgrade/TemporaryUpgrade/SpamBlocker.java
@@ -1,6 +1,8 @@
 package sep.server.model.game.cards.upgrade.TemporaryUpgrade;
 
 import sep.server.model.game.Player;
+import sep.server.model.game.cards.IPlayableCard;
+import sep.server.model.game.cards.damage.SpamDamage;
 
 public class SpamBlocker extends ATemporaryUpgrade{
 
@@ -12,5 +14,21 @@ public class SpamBlocker extends ATemporaryUpgrade{
 
     public void activateUpgrade(Player player) {
 
+        for (int i = 0; i < player.getPlayerHand().size(); i++) {
+            IPlayableCard currentCard = player.getPlayerHand().get(i);
+
+            if (currentCard instanceof SpamDamage) {
+
+                player.getPlayerHand().remove(i);
+                player.getAuthGameMode().getSpamDeck().add((SpamDamage) currentCard);
+
+                if (player.getPlayerDeck().isEmpty()) {
+                    player.shuffleAndRefillDeck();}
+
+                player.getPlayerHand().add(player.getPlayerDeck().remove(i));
+
+                i--;
+            }
+        }
     }
 }
