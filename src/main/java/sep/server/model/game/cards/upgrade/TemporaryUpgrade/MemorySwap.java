7ad239c97702d244f430cc520627504bb6d1173f
Jakob Zebhauser <jakob.zebhauser@web.de>
2024-01-16 17:23:26 +0100

    UpgradeKarte MemorySwap

--- src/main/java/sep/server/model/game/cards/upgrade/TemporaryUpgrade/MemorySwap.java
+++ src/main/java/sep/server/model/game/cards/upgrade/TemporaryUpgrade/MemorySwap.java
@@ -1,6 +1,9 @@
 package sep.server.model.game.cards.upgrade.TemporaryUpgrade;
 
 import sep.server.model.game.Player;
+import sep.server.model.game.cards.IPlayableCard;
+
+import java.util.ArrayList;
 
 public class MemorySwap extends ATemporaryUpgrade{
 
@@ -11,6 +14,25 @@ public class MemorySwap extends ATemporaryUpgrade{
     }
 
     public void activateUpgrade(Player player) {
+
+        ArrayList<IPlayableCard> drawnCardsFromDeck = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            if (player.getPlayerDeck().isEmpty()) {
+                player.shuffleAndRefillDeck();
+            }
+            IPlayableCard drawnCard = player.getPlayerDeck().remove(0);
+            drawnCardsFromDeck.add(drawnCard);
+        }
+
+        //TO-DO: Auswahl auf Clientseite
+        ArrayList<IPlayableCard> drawnCardsFromHand = new ArrayList<>();
+
+        player.getPlayerDeck().addAll(0, drawnCardsFromHand);
+
+        for (int i = 0; i < 3; i++) {
+            int handIndex = player.getPlayerHand().indexOf(drawnCardsFromHand.get(i));
+            player.getPlayerHand().set(handIndex, drawnCardsFromDeck.get(i));
+        }
 
     }
 }
