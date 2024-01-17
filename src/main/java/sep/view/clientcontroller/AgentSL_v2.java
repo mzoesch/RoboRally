d60bb787413e8eeccccc559ac159d289ffc68225
mzoesch <magnus.zoeschinger@zoeschinger.de>
2024-01-17 21:07:34 +0100

    Removed unnecessary Thread sleep

--- src/main/java/sep/view/clientcontroller/AgentSL_v2.java
+++ src/main/java/sep/view/clientcontroller/AgentSL_v2.java
@@ -416,16 +416,6 @@ public final class AgentSL_v2 extends ServerListener
                     return false;
                 }
 
-                /* We do this because else the server will get into a lot of race conditions. Pretty fishy. But there is no time to fix this. */
-                try
-                {
-                    Thread.sleep(200);
-                }
-                catch (final InterruptedException e)
-                {
-                    throw new RuntimeException(e);
-                }
-
                 l.debug("Sending starting point: {}.", location.toString());
                 new SetStartingPointModel(location.x(), location.y()).send();
 
