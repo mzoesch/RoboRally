4069ba0530e528f9de3a815c6a2c9b0dd1c6a96c
Jakob Zebhauser <jakob.zebhauser@web.de>
2024-01-17 14:26:17 +0100

    Log-Nachricht SpamBlocker

--- src/main/java/sep/server/model/game/cards/upgrade/TemporaryUpgrade/SpamBlocker.java
+++ src/main/java/sep/server/model/game/cards/upgrade/TemporaryUpgrade/SpamBlocker.java
@@ -1,10 +1,14 @@
 package sep.server.model.game.cards.upgrade.TemporaryUpgrade;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import sep.server.model.game.Player;
 import sep.server.model.game.cards.IPlayableCard;
 import sep.server.model.game.cards.damage.SpamDamage;
 
 public class SpamBlocker extends ATemporaryUpgrade{
+
+    private static final Logger logger = LogManager.getLogger(SpamBlocker.class);
 
     public SpamBlocker(String cardType, int cost) {
         super(cardType, cost);
@@ -13,6 +17,9 @@ public class SpamBlocker extends ATemporaryUpgrade{
     }
 
     public void activateUpgrade(Player player) {
+
+        logger.info("SpamBlocker activated for player '{}'. Removing SpamDamage card from hand.",
+                player.getController().getName());
 
         for (int i = 0; i < player.getPlayerHand().size(); i++) {
             IPlayableCard currentCard = player.getPlayerHand().get(i);
