28fe93642a884535ec5a33310cfc5f391e3c0e4d
Jakob Zebhauser <jakob.zebhauser@web.de>
2024-01-17 14:30:56 +0100

    Log-Nachricht admin Privilege

--- src/main/java/sep/server/model/game/cards/upgrade/PermanentUpgrade/AdminPrivilege.java
+++ src/main/java/sep/server/model/game/cards/upgrade/PermanentUpgrade/AdminPrivilege.java
@@ -1,8 +1,12 @@
 package sep.server.model.game.cards.upgrade.PermanentUpgrade;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import sep.server.model.game.Player;
 
 public class AdminPrivilege extends APermanentUpgrade{
+
+    private static final Logger logger = LogManager.getLogger(AdminPrivilege.class);
 
     public AdminPrivilege(String cardType, int cost) {
         super(cardType, cost);
@@ -13,7 +17,9 @@ public class AdminPrivilege extends APermanentUpgrade{
     @Override
     public void activateUpgrade(Player player) {
 
+        logger.info("Update Card (AdminPrivilege) activated for player '{}'. Setting hasAdminPrivilegeUpgrade to true.",
+                player.getController().getName());
 
-
+        player.setHasAdminPrivilegeUpgrade(true);
     }
 }
