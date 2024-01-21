c078b0c95d7aed4fd212e6376e98c0920ee50204
Lea Allinger <l.allinger@campus.lmu.de>
2024-01-21 17:41:02 +0100

    Added javadoc to EnergyRoutine.java

--- src/main/java/sep/server/model/game/cards/special/EnergyRoutine.java
+++ src/main/java/sep/server/model/game/cards/special/EnergyRoutine.java
@@ -1,15 +1,25 @@
 package sep.server.model.game.cards.special;
 
-import sep.server.json.game.effects.EnergyModel;
 import sep.server.model.game.Player;
 import sep.server.model.game.cards.IPlayableCard;
 
+/**
+ * Represents a special programming card with an energy routine effect.
+ * The EnergyRoutine card increases the player's energy by 1 and broadcasts the energy update.
+ */
 public class EnergyRoutine extends ASpecialProgrammingCard implements IPlayableCard {
+
     public EnergyRoutine(String cardType) {
         super(cardType);
         this.cardType = "EnergyRoutine";
     }
 
+    /**
+     * Plays the EnergyRoutine card, increasing the player's energy by 1 and broadcasting the energy update.
+     *
+     * @param player             the player playing the card
+     * @param currentRoundNumber the current round number
+     */
     @Override
     public void playCard(Player player, int currentRoundNumber){
         int currentEnergy = player.getEnergyCollected();
@@ -17,6 +27,5 @@ public class EnergyRoutine extends ASpecialProgrammingCard implements IPlayableC
         player.setEnergyCollected(newEnergy);
 
         player.getAuthGameMode().getSession().broadcastEnergyUpdate(player.getController().getPlayerID(), newEnergy, "EnergyRoutine");
-
     }
 }
