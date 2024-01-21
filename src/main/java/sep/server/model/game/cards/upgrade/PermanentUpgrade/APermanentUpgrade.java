2e08edce365b0189261c971474c26eb7e88f897b
Lea Allinger <l.allinger@campus.lmu.de>
2024-01-21 18:08:34 +0100

    Added javadoc to APermanentUpgrade.java

--- src/main/java/sep/server/model/game/cards/upgrade/PermanentUpgrade/APermanentUpgrade.java
+++ src/main/java/sep/server/model/game/cards/upgrade/PermanentUpgrade/APermanentUpgrade.java
@@ -3,7 +3,12 @@ package sep.server.model.game.cards.upgrade.PermanentUpgrade;
 import sep.server.model.game.Player;
 import sep.server.model.game.cards.upgrade.AUpgradeCard;
 
+/**
+ * Represents an abstract permanent upgrade card.
+ * Permanent upgrades have a cost and provide lasting effects when activated.
+ */
 public abstract class APermanentUpgrade extends AUpgradeCard {
+
     public APermanentUpgrade(String cardType, int cost) {
         super(cardType, cost);
     }
