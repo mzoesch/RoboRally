78ab9b5dc17e252b50371cf7f10a2b7cf0c8a77c
Lea Allinger <l.allinger@campus.lmu.de>
2024-01-21 17:37:08 +0100

    Added logging and JavaDoc comments to TurnRight.java

--- src/main/java/sep/server/model/game/cards/programming/TurnRight.java
+++ src/main/java/sep/server/model/game/cards/programming/TurnRight.java
@@ -1,21 +1,36 @@
 package sep.server.model.game.cards.programming;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import sep.server.model.game.GameState;
 import sep.server.model.game.Player;
 import sep.server.model.game.cards.IPlayableCard;
 
+/**
+ * Represents a programming card that rotates the player's robot to the right.
+ */
 public class TurnRight extends AProgrammingCard implements IPlayableCard {
+
+    private static final Logger l = LogManager.getLogger(GameState.class);
 
     public TurnRight(String cardType) {
         super(cardType);
         this.cardType = "TurnRight";
     }
 
+    /**
+     * Plays the TurnLeft programming card, rotating the player's robot to the left.
+     *
+     * @param player             the player playing the card
+     * @param currentRoundNumber the current round number
+     */
     @Override
     public void playCard(Player player, int currentRoundNumber) {
         player.getPlayerRobot().rotateRobotOnTileToTheRight();
 
         if(!player.getPlayerRobot().isRebootTriggered()) {
             player.getAuthGameMode().getSession().broadcastRotationUpdate(player.getController().getPlayerID(), "clockwise");
+            l.info("Player {} played TurnRight card, rotating robot clockwise.", player.getController().getPlayerID());
         } else {
             player.getPlayerRobot().setRebootTriggered(false);
         }
