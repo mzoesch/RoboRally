f46d05098848b1c05d00415a7cd54f09bdac26b2
Lea Allinger <l.allinger@campus.lmu.de>
2024-01-21 17:38:37 +0100

    Added logging and JavaDoc comments to UTurn.java

--- src/main/java/sep/server/model/game/cards/programming/UTurn.java
+++ src/main/java/sep/server/model/game/cards/programming/UTurn.java
@@ -1,15 +1,29 @@
 package sep.server.model.game.cards.programming;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import sep.server.model.game.GameState;
 import sep.server.model.game.Player;
 import sep.server.model.game.cards.IPlayableCard;
 
+/**
+ * Represents a programming card that performs a U-turn, rotating the player's robot 180 degrees.
+ */
 public class UTurn extends AProgrammingCard implements IPlayableCard {
+
+    private static final Logger l = LogManager.getLogger(GameState.class);
 
     public UTurn(String cardType) {
         super(cardType);
         this.cardType = "UTurn";
     }
 
+    /**
+     * Plays the UTurn programming card, rotating the player's robot 180 degrees.
+     *
+     * @param player             the player playing the card
+     * @param currentRoundNumber the current round number
+     */
     @Override
     public void playCard(Player player, int currentRoundNumber) {
         player.getPlayerRobot().rotateRobotOnTileToTheRight();
@@ -18,6 +32,7 @@ public class UTurn extends AProgrammingCard implements IPlayableCard {
         if(!player.getPlayerRobot().isRebootTriggered()) {
             player.getAuthGameMode().getSession().broadcastRotationUpdate(player.getController().getPlayerID(), "clockwise");
             player.getAuthGameMode().getSession().broadcastRotationUpdate(player.getController().getPlayerID(), "clockwise");
+            l.info("Player {} played UTurn card, performing a 180-degree rotation.", player.getController().getPlayerID());
         } else {
             player.getPlayerRobot().setRebootTriggered(false);
         }
