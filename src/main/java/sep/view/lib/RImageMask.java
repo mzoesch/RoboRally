65b4a98cb17e80ec71087bbaebcb98cbb7751707
mzoesch <magnus.zoeschinger@zoeschinger.de>
2024-01-25 16:14:25 +0100

    Typo

--- src/main/java/sep/view/lib/RImageMask.java
+++ src/main/java/sep/view/lib/RImageMask.java
@@ -6,7 +6,7 @@ import javafx.scene.image.  Image;
 
 public final record RImageMask(Image i, String url)
 {
-    /* We store the url outside the JFX Image because by translating from the SWING Util Library, the url will be discarded. */
+    /* We store the url outside the JFX Image because by translating from the SWING Util Library, the URL will be discarded. */
 
     /** Will always return a valid image url. */
     public String getSanitizedURL()
