466f9163c47f650d3e39ab4416abc6f360c5d818
Julian Hofmann <jthofmann@posteo.de>
2024-01-27 09:02:24 +0100

    Fixed wrong directions for RestartPoints in CourseBuilder

--- src/main/java/sep/server/model/game/builder/CourseBuilder.java
+++ src/main/java/sep/server/model/game/builder/CourseBuilder.java
@@ -35,7 +35,7 @@ public class CourseBuilder {
             case("Lost Bearings") -> {
                 ArrayList<ArrayList<Tile>> boardStartA = buildBoard("StartA");
                 ArrayList<ArrayList<Tile>> board1A = buildBoard("1A");
-                changeFieldType(boardStartA, 0, 0, new RestartPoint("top"));
+                changeFieldType(boardStartA, 0, 0, new RestartPoint("right"));
                 ArrayList<ArrayList<Tile>> entireCourse = appendRight(boardStartA, board1A);
                 settingCoordinates(entireCourse);
                 return entireCourse;
@@ -43,7 +43,7 @@ public class CourseBuilder {
             case("Extra Crispy") -> {
                 ArrayList<ArrayList<Tile>> boardStartA = buildBoard("StartA");
                 ArrayList<ArrayList<Tile>> board4A = buildBoard("4A");
-                changeFieldType(boardStartA, 0, 0, new RestartPoint("top"));
+                changeFieldType(boardStartA, 0, 0, new RestartPoint("right"));
                 ArrayList<ArrayList<Tile>> entireCourse = appendRight(boardStartA, board4A);
                 settingCoordinates(entireCourse);
                 return entireCourse;
@@ -51,7 +51,7 @@ public class CourseBuilder {
             case("Death Trap") -> {
                 ArrayList<ArrayList<Tile>> boardStartA = buildBoard("StartA");
                 ArrayList<ArrayList<Tile>> board2A = buildBoard("2A");
-                changeFieldType(boardStartA, 0, 9, new RestartPoint("bottom"));
+                changeFieldType(boardStartA, 0, 9, new RestartPoint("left"));
                 changeFieldType(boardStartA, 2, 9, new ConveyorBelt(1, "left", new String[] {"right"}));
                 changeFieldType(boardStartA, 2, 0, new ConveyorBelt(1, "left", new String[] {"right"}));
                 changeFieldType(boardStartA, 2, 4, new Wall(new String[]{"left"}));
@@ -66,7 +66,7 @@ public class CourseBuilder {
             case("Twister") -> {
                 ArrayList<ArrayList<Tile>> boardStartA = buildBoard("StartA");
                 ArrayList<ArrayList<Tile>> board6B = buildBoard("6B");
-                changeFieldType(boardStartA, 0, 7, new RestartPoint("top"));
+                changeFieldType(boardStartA, 0, 7, new RestartPoint("right"));
                 ArrayList<ArrayList<Tile>> entireCourse = appendRight(boardStartA, board6B);
                 settingCoordinates(entireCourse);
                 return entireCourse;
