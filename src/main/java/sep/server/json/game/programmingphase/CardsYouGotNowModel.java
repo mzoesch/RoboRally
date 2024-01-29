a3ee51719bc31b5b89a63161f1f5a343aae71a73
mzoesch <magnus.zoeschinger@zoeschinger.de>
2024-01-29 12:56:05 +0100

    Refactored CardsYouGotNowModel

--- src/main/java/sep/server/json/game/programmingphase/CardsYouGotNowModel.java
+++ src/main/java/sep/server/json/game/programmingphase/CardsYouGotNowModel.java
@@ -1,27 +1,35 @@
 package sep.server.json.game.programmingphase;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import sep.server.json.AModel;
-import sep.server.viewmodel.ClientInstance;
+import sep.server.json.         AModel;
+import sep.server.viewmodel.    ClientInstance;
 
-public class CardsYouGotNowModel extends AModel {
+import org.json.    JSONArray;
+import org.json.    JSONObject;
 
+public final class CardsYouGotNowModel extends AModel
+{
     private final String[] cards;
 
-    public CardsYouGotNowModel(ClientInstance ci, String[] cards) {
+    public CardsYouGotNowModel(final ClientInstance ci, final String[] cards)
+    {
         super(ci);
+
         this.cards = cards;
+
+        return;
     }
 
     @Override
-    public JSONObject toJSON() {
-        JSONObject body = new JSONObject();
-        body.put("cards", new JSONArray(cards));
+    public JSONObject toJSON()
+    {
+        final JSONObject body = new JSONObject();
+        body.put(   "cards",    new JSONArray(this.cards)   );
 
-        JSONObject j = new JSONObject();
-        j.put("messageType", "CardsYouGotNow");
-        j.put("messageBody",body);
+        final JSONObject j = new JSONObject();
+        j.put(  "messageType",      "CardsYouGotNow"        );
+        j.put(  "messageBody",      body                    );
+
         return j;
     }
+
 }
