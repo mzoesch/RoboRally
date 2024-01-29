c34dcf6d21729deac2c579a2a71d1de7f8975885
mzoesch <magnus.zoeschinger@zoeschinger.de>
2024-01-29 12:56:49 +0100

    Refactored TimerStartedModel

--- src/main/java/sep/server/json/game/programmingphase/TimerStartedModel.java
+++ src/main/java/sep/server/json/game/programmingphase/TimerStartedModel.java
@@ -1,21 +1,24 @@
 package sep.server.json.game.programmingphase;
 
-import org.json.JSONObject;
-import sep.server.json.AModel;
-import sep.server.viewmodel.ClientInstance;
+import sep.server.json.         AModel;
+import sep.server.viewmodel.    ClientInstance;
 
-public class TimerStartedModel extends AModel {
+import org.json.    JSONObject;
 
-
-    public TimerStartedModel(ClientInstance ci) {
+public final class TimerStartedModel extends AModel
+{
+    public TimerStartedModel(final ClientInstance ci)
+    {
         super(ci);
+        return;
     }
 
     @Override
-    public JSONObject toJSON() {
-        JSONObject j = new JSONObject();
+    public JSONObject toJSON()
+    {
+        final JSONObject j = new JSONObject();
         j.put("messageType", "TimerStarted");
-
         return j;
     }
+
 }
