6c28e6da6650cf6737aff9836d28a5ae61b03586
mzoesch <magnus.zoeschinger@zoeschinger.de>
2024-02-02 18:13:09 +0100

    Added RPushPanelMask

---
+++ src/main/java/sep/view/lib/RPushPanelMask.java
@@ -0,0 +1,8 @@
+package sep.view.lib;
+
+import java.util.           ArrayList;
+import javafx.scene.image.  ImageView;
+
+public final record RPushPanelMask(ImageView iv, RRotation rot, ArrayList<Integer> registers)
+{
+}
